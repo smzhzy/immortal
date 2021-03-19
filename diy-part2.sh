d75b490e3610d76e14ac5c1e28052cb243f96d75
smzhzy <xiasonming@163.com>
2021-03-19 20:57:21 +0800

    initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# 把默认bootstrap主题改成material
+# sed -i 's/OpenWrt/xsm/g' package/base-files/files/bin/config_generate
+
+exit 0
